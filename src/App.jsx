42fa111001b0be71607172afaeb7a473a52d4b7d
John Dick <johnturnerdick@gmail.com>
2026-04-04 21:12:31 +0100

    initial commit for my React & Tailwind CSS test project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-5xl font-bold text-sky-400 underline decoration-pink-500">
+        Tailwind v4 + React - Hello World
+      </h1>
+    </div>
+  )
+}
+
+
+export default App
