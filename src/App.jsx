ac41e7aa02c0ac2e2924956add42a96e6f3553f3
John Dick <johnturnerdick@gmail.com>
2026-04-06 21:14:36 +0100

    added border for debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div className="min-h-dvh bg-white text-slate-800 antialiased">
-      <header className="flex min-h-dvh flex-col justify-start px-6 sm:px-10">
-        <div className="mx-auto mt-[150px] w-full max-w-lg text-center sm:text-left">
+    <div className="min-h-dvh border-2 border-dashed border-rose-400 bg-white text-slate-800 antialiased">
+      <header className="flex min-h-dvh flex-col justify-start border-2 border-dashed border-sky-400 px-6 sm:px-10">
+        <div className="mx-auto mt-[150px] w-full max-w-lg border-2 border-dashed border-amber-400 text-center sm:text-left">
           <h1 className="text-balance text-3xl font-semibold leading-tight tracking-tight text-slate-900 sm:text-4xl">
             React application test
           </h1>
